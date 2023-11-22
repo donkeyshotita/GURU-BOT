c879f396c48d8a2d9358c015aa0f0e63ef63461b
donkeyshotita <151694924+donkeyshotita@users.noreply.github.com>
2023-11-22 02:20:28 +0100

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
+  ['18035805322', 'InsideItaly', true],
   ['6281255369012', 'Ash', true], 
   [''] 
 ] //Number of owners
